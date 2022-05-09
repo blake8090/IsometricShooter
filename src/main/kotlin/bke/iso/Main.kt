991945818b904e312f17dd23b68962819f93b3a1
Blake Edwards <blake8090@gmail.com>
2022-05-08 23:55:22 -0400

    Initial commit

---
+++ src/main/kotlin/bke/iso/Main.kt
@@ -0,0 +1,12 @@
+package bke.iso
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+
+fun main() {
+    Lwjgl3Application(
+        object : ApplicationAdapter() {},
+        Lwjgl3ApplicationConfiguration()
+    )
+}
