feebbfc0e1a9216673b046f31bafa630bd39e039
Blake Edwards <blake8090@gmail.com>
2022-05-18 01:56:12 -0400

    Rewrite IocContainer

--- src/main/kotlin/bke/iso/IocContainer.kt
+++ src/main/kotlin/bke/iso/IocContainer.kt
@@ -11,11 +11,6 @@ class MissingBindingException(message: String) : RuntimeException(message)
 class MissingConstructorException(message: String) : RuntimeException(message)
 class DuplicateBindingException(message: String) : RuntimeException(message)
 
-data class Binding<T : Any>(
-    val interfaceClass: KClass<T>,
-    val implementationClass: KClass<out T>
-)
-
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class Service
@@ -23,62 +18,85 @@ annotation class Service
 // TODO: add unit tests
 class IocContainer {
     private val log = LoggerFactory.getLogger(IocContainer::class.java)
-    private val bindingMap = mutableMapOf<KClass<*>, Binding<*>>()
+
+    private val implementationByInterfaceMap = mutableMapOf<KClass<*>, KClass<*>>()
     private val instanceMap = mutableMapOf<KClass<*>, Any>()
 
     init {
-        registerService<IocContainer>()
+        registerService(IocContainer::class)
+        instanceMap[IocContainer::class] = this
     }
 
+    /**
+     * Searches the given [classPath] for classes annotated with [Service] and registers them as services.
+     */
     fun registerFromClassPath(classPath: String) {
         Reflections(classPath, Scanners.TypesAnnotated)
             .getTypesAnnotatedWith(Service::class.java)
             .filterNotNull()
-            .map { clazz -> clazz.kotlin }
-            .forEach { clazz -> registerService(clazz) }
+            .forEach { clazz -> registerService(clazz.kotlin) }
     }
 
-    fun <T : Any> registerService(binding: Binding<T>) {
-        if (bindingMap.containsKey(binding.interfaceClass)) {
-            throw DuplicateBindingException("Class ${binding.interfaceClass.simpleName} has already been registered")
+    /**
+     * Given an [implementationClass], registers a service with the interface the same as the implementation.
+     */
+    fun <T : Any> registerService(implementationClass: KClass<T>) =
+        registerService(implementationClass, implementationClass)
+
+    /**
+     * Registers a service with the interface as [interfaceClass] and the implementation as [implementationClass].
+     *
+     * @throws DuplicateBindingException if the interface class has already been registered
+     */
+    fun <T : Any, U : T> registerService(interfaceClass: KClass<T>, implementationClass: KClass<U>) {
+        if (implementationByInterfaceMap.containsKey(interfaceClass)) {
+            throw DuplicateBindingException(
+                "Interface class ${interfaceClass.simpleName} has already been"
+                        + " registered to class ${implementationClass.simpleName}"
+            )
         }
-        bindingMap[binding.interfaceClass] = binding
+        implementationByInterfaceMap[interfaceClass] = implementationClass
         log.debug(
             "Registered implementation '{}' for interface '{}'",
-            binding.implementationClass.simpleName,
-            binding.interfaceClass.simpleName
+            implementationClass.simpleName,
+            interfaceClass.simpleName
         )
     }
 
-    fun <T : Any> registerService(clazz: KClass<T>) {
-        registerService(Binding(clazz, clazz))
-    }
-
-    inline fun <reified T : Any> registerService() =
-        registerService(T::class)
-
-    fun <T : Any> getService(interfaceClass: KClass<T>): T {
-        val binding = bindingMap[interfaceClass]
-            ?: throw MissingBindingException("No binding found for class ${interfaceClass.simpleName}")
-        val serviceClass = binding.implementationClass
-        return interfaceClass.cast(instanceMap.getOrPut(serviceClass) { createInstance(serviceClass) })
-    }
-
+    /**
+     * Returns an instance of the service [T].
+     *
+     * @throws MissingBindingException if no service exists for class [T]
+     */
     inline fun <reified T : Any> getService(): T = getService(T::class)
 
-    private fun <T : Any> createInstance(implementationClass: KClass<out T>): T {
-        log.trace("Creating instance for implementation class {}", implementationClass.simpleName)
+    /**
+     * Returns an instance of a service with the given [interfaceClass].
+     *
+     * @throws MissingBindingException if no service exists for the [interfaceClass]
+     */
+    fun <T : Any> getService(interfaceClass: KClass<T>): T {
+        val implementationClass = implementationByInterfaceMap[interfaceClass]
+            ?: throw MissingBindingException("No binding found for class ${interfaceClass.simpleName}")
+        val instance = instanceMap.getOrPut(implementationClass) { createInstance(implementationClass) }
+        return interfaceClass.cast(instance)
+    }
 
-        val constructor = implementationClass.constructors.firstOrNull()
+    /**
+     * Creates an instance of the given [implementationClass], and injects all dependencies in the constructor.
+     */
+    fun <T : Any> createInstance(implementationClass: KClass<out T>): T {
+        log.trace("Creating instance for implementation class '$implementationClass.simpleName'")
+
+        val constructor = implementationClass.constructors
+            .firstOrNull()
             ?: throw MissingConstructorException("Expected constructor for class '${implementationClass.simpleName}'")
 
-        val dependencies = constructor.parameters.map { parameter -> parameter.type.classifier as KClass<*> }
-            .map { instanceClass -> getService(instanceClass) }.toTypedArray()
+        val dependencies = constructor.parameters
+            .map { parameter -> parameter.type.classifier as KClass<*> }
+            .map { instanceClass -> getService(instanceClass) }
+            .toTypedArray()
 
-        return if (dependencies.isNotEmpty()) {
-            constructor.call(*dependencies)
-        } else {
-            constructor.call()
-        }
+        return constructor.call(*dependencies)
     }
 }
