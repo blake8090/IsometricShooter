f812ab6c843816dd4da36d8879ffaea5011e9e30
Blake Edwards <blake8090@gmail.com>
2022-05-23 18:27:20 -0400

    delete unused class

--- src/main/kotlin/bke/iso/asset/AssetCache.kt
+++
@@ -1,9 +0,0 @@
-package bke.iso.asset
-
-class AssetCache<T : Any> {
-    private val cache = mutableMapOf<String, T>()
-
-    operator fun set(name: String, asset: T) {
-        cache[name] = asset
-    }
-}
