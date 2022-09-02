3071aa9b4fdc8feb756134d590aed22ff3a4bc51
Blake Edwards <blake8090@gmail.com>
2022-09-02 19:00:00 -0400

    implement v2 services

---
+++ src/main/kotlin/bke/iso/v2/app/ServiceCache.kt
@@ -0,0 +1,25 @@
+package bke.iso.v2.app
+
+import java.lang.RuntimeException
+import kotlin.reflect.KClass
+import kotlin.reflect.safeCast
+
+class MissingServiceException(message: String) : RuntimeException(message)
+
+class ServiceCache {
+    private val services = mutableMapOf<KClass<*>, Lazy<*>>()
+
+    fun <T : Any> add(type: KClass<T>, provider: () -> T) {
+        services[type] = lazy(provider)
+    }
+
+    fun <T : Any> get(type: KClass<T>): T {
+        val instance = services[type]
+            ?.value
+            ?: throw MissingServiceException("Service ${type.simpleName} was not found")
+        return type.safeCast(instance)
+            ?: throw IllegalArgumentException(
+                "Instance ${instance.javaClass.simpleName} is not of type ${type.simpleName}"
+            )
+    }
+}
