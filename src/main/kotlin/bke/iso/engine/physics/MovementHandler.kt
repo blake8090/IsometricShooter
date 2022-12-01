0a4613f715cfc9070aa6df663a63f1ad8b3119a2
Blake Edwards <blake8090@gmail.com>
2022-11-30 23:24:51 -0500

    merge collision resolution code into MovementHandler

--- src/main/kotlin/bke/iso/engine/physics/MovementHandler.kt
+++ src/main/kotlin/bke/iso/engine/physics/MovementHandler.kt
@@ -2,15 +2,53 @@ package bke.iso.engine.physics
 
 import bke.iso.engine.Engine
 import bke.iso.engine.event.EventHandler
+import bke.iso.engine.log
 
 class MovementHandler(
     private val engine: Engine,
-    private val physicsService: PhysicsService
+    private val collisionService: CollisionService
 ) : EventHandler<MoveEvent> {
     override fun handle(event: MoveEvent) {
         val entity = event.entity
-        val dx = event.dx * engine.deltaTime
-        val dy = event.dy * engine.deltaTime
-        physicsService.move(entity, dx, dy)
+        var dx = event.dx * engine.deltaTime
+        var dy = event.dy * engine.deltaTime
+
+        val result = collisionService.checkProjectedCollisions(entity, dx, dy)
+        if (result != null) {
+            val bounds = result.bounds
+            for (solidCollision in result.solidCollisions) {
+                val solidArea = solidCollision.area
+                when (solidCollision.side) {
+                    CollisionSide.TOP -> {
+                        entity.y = solidArea.y - bounds.length - bounds.offsetY
+                        dy = 0f
+                    }
+
+                    CollisionSide.BOTTOM -> {
+                        entity.y = (solidArea.y + solidArea.height) - bounds.offsetY
+                        dy = 0f
+                    }
+
+                    CollisionSide.LEFT -> {
+                        entity.x = (solidArea.x + solidArea.width) - bounds.offsetX
+                        dx = 0f
+                    }
+
+                    CollisionSide.RIGHT -> {
+                        entity.x = solidArea.x - bounds.length - bounds.offsetX
+                        dx = 0f
+                    }
+
+                    else -> {
+                        log.trace("Corner collision...?")
+                        dx = 0f
+                        dy = 0f
+                    }
+                }
+            }
+        }
+
+        entity.x += dx
+        entity.y += dy
     }
 }
