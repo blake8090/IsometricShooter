451e65d0eb1bfc1e6696e6e8293746921691e87a
Blake Edwards <blake8090@gmail.com>
2023-01-24 22:31:50 -0500

    refactor TileService to use z-axis

--- src/main/kotlin/bke/iso/engine/Tiles.kt
+++ src/main/kotlin/bke/iso/engine/Tiles.kt
@@ -3,25 +3,42 @@ package bke.iso.engine
 import bke.iso.service.Singleton
 import bke.iso.engine.math.Location
 import bke.iso.engine.render.Sprite
+import java.util.TreeSet
 
 data class Tile(val sprite: Sprite)
 
+private typealias TileMap = TreeSet<TileRecord>
+
 @Singleton
 class TileService {
-    private val tileByLocation = mutableMapOf<Location, Tile>()
-        .toSortedMap(
-            compareByDescending(Location::y)
-                .thenBy(Location::x)
-                .thenByDescending(Location::z)
-        )
+
+    private val layers = sortedMapOf<Int, TileMap>()
 
     fun setTile(tile: Tile, location: Location) {
-        tileByLocation[location] = tile
+        val tileMap = layers.getOrPut(location.z) { createTileMap() }
+        val record = TileRecord(location, tile)
+        if (tileMap.contains(record)) {
+            tileMap.remove(record)
+        }
+        tileMap.add(record)
     }
 
+    private fun createTileMap(): TileMap =
+        TreeSet<TileRecord>(
+            compareByDescending<TileRecord> { record -> record.location.y }
+                .thenBy { record -> record.location.x }
+        )
+
     fun forEachTile(action: (Location, Tile) -> Unit) {
-        tileByLocation.forEach { (location, tile) ->
-            action.invoke(location, tile)
+        for ((_, layer) in layers) {
+            for ((location, tile) in layer) {
+                action.invoke(location, tile)
+            }
         }
     }
 }
+
+private data class TileRecord(
+    val location: Location,
+    val tile: Tile
+)
