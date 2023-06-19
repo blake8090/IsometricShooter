f4b5b4056e86902b11a163d2abfeabe88a09347a
Blake Edwards <blake8090@gmail.com>
2023-06-19 16:09:23 -0400

    add collision models

--- src/main/kotlin/bke/iso/engine/physics/collision/EntityCollisionData.kt
+++
@@ -1,7 +0,0 @@
-package bke.iso.engine.physics.collision
-
-data class EntityCollisionData(
-    val bounds: BoundsV2,
-    val box: Box,
-    val solid: Boolean
-)
---
+++ src/main/kotlin/bke/iso/engine/physics/collision/Models.kt
@@ -0,0 +1,38 @@
+package bke.iso.engine.physics.collision
+
+import bke.iso.engine.entity.Entity
+import com.badlogic.gdx.math.Vector3
+
+data class EntityCollisionData(
+    val bounds: BoundsV2,
+    val box: Box,
+    val solid: Boolean
+)
+
+enum class BoxCollisionSide {
+    FRONT,
+    BACK,
+    LEFT,
+    RIGHT,
+    TOP,
+    BOTTOM
+}
+
+data class EntityBoxCollision(
+    val entity: Entity,
+    val data: EntityCollisionData,
+    val side: BoxCollisionSide
+)
+
+data class ProjectedCollisions(
+    val projectedBox: Box,
+    val collisions: Set<EntityBoxCollision>
+)
+
+data class EntitySegmentCollision(
+    val entity: Entity,
+    val data: EntityCollisionData,
+    val distanceFromStart: Float,
+    val distanceFromEnd: Float,
+    val intersectionPoints: Set<Vector3>
+)
