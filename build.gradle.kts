529fd2fc52f23ffc6fed5c39cf99a5944fd64bf8
Blake Edwards <blake8090@gmail.com>
2023-07-29 21:21:53 -0400

    upgrade to kotlin 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.kotlin.dsl.application
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.8.20"
+    id("org.jetbrains.kotlin.jvm") version "1.9.0"
     application
 }
 
@@ -13,19 +13,18 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-
-    implementation("io.github.libktx:ktx-async:1.10.0-rc1")
     implementation("org.reflections:reflections:0.10.2")
+
+    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:1.11.0")
+    implementation("com.badlogicgames.gdx:gdx-platform:1.11.0:natives-desktop")
+    implementation("io.github.libktx:ktx-async:1.10.0-rc1")
+    implementation("space.earlygrey:shapedrawer:2.5.0")
 
     implementation("ch.qos.logback:logback-core:1.4.6")
     implementation("ch.qos.logback:logback-classic:1.4.6")
     implementation("org.slf4j:slf4j-api:2.0.7")
-
-    implementation("com.badlogicgames.gdx:gdx-backend-lwjgl3:1.11.0")
-    implementation("com.badlogicgames.gdx:gdx-platform:1.11.0:natives-desktop")
-    implementation("space.earlygrey:shapedrawer:2.5.0")
 
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.14.2")
     implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
