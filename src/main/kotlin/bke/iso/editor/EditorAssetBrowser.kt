c5f17eb0942d815d127c107d7e6cb1b0b429b9fb
Blake Edwards <blake8090@gmail.com>
2023-09-07 11:57:09 -0400

    split asset browser into own class

---
+++ src/main/kotlin/bke/iso/editor/EditorAssetBrowser.kt
@@ -0,0 +1,80 @@
+package bke.iso.editor
+
+import bke.iso.engine.asset.Assets
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.Actor
+import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup
+import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
+import com.badlogic.gdx.scenes.scene2d.ui.ImageTextButton
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+
+class EditorAssetBrowser(private val assets: Assets) {
+
+    fun create(skin: Skin): Actor {
+        val root = Table().top().left()
+
+        val tabs = HorizontalGroup()
+        val tileButton = textButton("Tiles", skin, "sidebarTab")
+        tileButton.padLeft(16f)
+        tileButton.padRight(16f)
+        tabs.addActor(tileButton)
+
+        val actorButton = textButton("Actors", skin, "sidebarTab")
+        actorButton.padLeft(16f)
+        actorButton.padRight(16f)
+        tabs.addActor(actorButton)
+
+        ButtonGroup<TextButton>().add(tileButton, actorButton)
+        root.add(tabs).left()
+
+        root.row()
+        val container = BorderedTable(color(77, 100, 130))
+        container.borderSize = 4f
+
+        val browser = Table().top().left()
+        browser.add(createAssetButton("floor", "game/gfx/tiles/floor", skin))
+            .fill()
+
+        val scrollPane = ScrollPane(browser)
+        scrollPane.layout()
+        container.add(scrollPane)
+            .fill()
+            .expand()
+
+        root.add(container)
+            .fill()
+            .expand()
+
+        return root
+    }
+
+    private fun createAssetButton(name: String, texture: String, skin: Skin): ImageTextButton {
+        val style = ImageTextButton.ImageTextButtonStyle().apply {
+            imageUp = getTextureDrawable(texture)
+            over = skin.getDrawable("toolbar-over")
+            down = skin.getDrawable("toolbar-down")
+            checked = skin.getDrawable("toolbar-checked")
+            font = skin.getFont("font")
+        }
+
+        val button = ImageTextButton(name, style)
+        // align label to bottom instead of right by default
+        val img = button.image
+        val label = button.label
+        button.clearChildren()
+        button.add(img)
+        button.row()
+        button.add(label)
+        return button
+    }
+
+    private fun getTextureDrawable(name: String): TextureRegionDrawable {
+        val texture = assets.get<Texture>(name)
+        return TextureRegionDrawable(TextureRegion(texture))
+    }
+}
--- src/main/kotlin/bke/iso/editor/EditorScreen.kt
+++ src/main/kotlin/bke/iso/editor/EditorScreen.kt
@@ -7,21 +7,19 @@ import bke.iso.engine.ui.UIScreen
 import bke.iso.engine.util.TextButtonBuilder
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.ButtonGroup
-import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton
 import com.badlogic.gdx.scenes.scene2d.ui.Label
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.ui.Value
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
-import com.badlogic.gdx.utils.Align
 
 class EditorScreen(private val assets: Assets) : UIScreen() {
+
+    private val assetBrowser = EditorAssetBrowser(assets)
 
     override fun create() {
         setup()
@@ -119,68 +117,9 @@ class EditorScreen(private val assets: Assets) : UIScreen() {
         sideBar.borderSize = 2f
         sideBar.background = skin.getDrawable("bg")
 
-        val tabs = HorizontalGroup()
-        val tileButton = textButton("Tiles", skin, "sidebarTab")
-        tileButton.padLeft(16f)
-        tileButton.padRight(16f)
-        tabs.addActor(tileButton)
-
-        val actorButton = textButton("Actors", skin, "sidebarTab")
-        actorButton.padLeft(16f)
-        actorButton.padRight(16f)
-        tabs.addActor(actorButton)
-
-        ButtonGroup<TextButton>().add(tileButton, actorButton)
-        sideBar.add(tabs).left()
-
-        sideBar.row()
-        val assetBrowserContainer = BorderedTable(color(77, 100, 130))
-        assetBrowserContainer.borderSize = 6f
-        sideBar.add(assetBrowserContainer)
+        sideBar.add(assetBrowser.create(skin))
             .expand()
             .fill()
-
-        val assetBrowser = BorderedTable(color(77, 100, 130))
-        assetBrowser.top().left()
-        assetBrowser.borderSize = 6f
-
-        val testAsset = ImageButton(getTextureDrawable("game/gfx/objects/fence-front"))
-        testAsset.style.over = skin.getDrawable("toolbar-over")
-        testAsset.style.down = skin.getDrawable("toolbar-down")
-        testAsset.style.checked = skin.getDrawable("toolbar-checked")
-        testAsset.layout()
-        testAsset.image.setOrigin(Align.center)
-        assetBrowser.add(testAsset)
-            .width(Value.percentWidth(.333f, assetBrowser))
-            .fill()
-
-        val testAsset2 = ImageButton(getTextureDrawable("game/gfx/objects/player"))
-        testAsset2.style.over = skin.getDrawable("toolbar-over")
-        testAsset2.style.down = skin.getDrawable("toolbar-down")
-        testAsset2.style.checked = skin.getDrawable("toolbar-checked")
-        testAsset2.layout()
-        testAsset2.image.setOrigin(Align.center)
-        assetBrowser.add(testAsset2)
-            .width(Value.percentWidth(.333f, assetBrowser))
-            .fill()
-
-        val testAsset3 = ImageButton(getTextureDrawable("game/gfx/objects/box"))
-        testAsset3.style.over = skin.getDrawable("toolbar-over")
-        testAsset3.style.down = skin.getDrawable("toolbar-down")
-        testAsset3.style.checked = skin.getDrawable("toolbar-checked")
-        testAsset3.layout()
-        testAsset3.image.setOrigin(Align.center)
-        assetBrowser.add(testAsset3)
-            .width(Value.percentWidth(.333f, assetBrowser))
-            .fill()
-
-        val scrollStyle = ScrollPane.ScrollPaneStyle()
-        scrollStyle.hScrollKnob = getTextureDrawable("ui/editor/pointer")
-        val scrollPane = ScrollPane(assetBrowser, scrollStyle)
-        assetBrowserContainer.add(scrollPane)
-            .fill()
-            .expand()
-        scrollPane.layout()
 
         return sideBar
     }
@@ -258,48 +197,5 @@ class EditorScreen(private val assets: Assets) : UIScreen() {
     private fun getTextureDrawable(name: String): TextureRegionDrawable {
         val texture = assets.get<Texture>(name)
         return TextureRegionDrawable(TextureRegion(texture))
-    }
-
-    /**
-     * Utility to create a [Color] from RGBA values provided in a 0 to 255 range
-     */
-    private fun color(r: Int, g: Int, b: Int, a: Int = 255): Color =
-        Color(
-            (r / 255f).coerceIn(0f, 1f),
-            (g / 255f).coerceIn(0f, 1f),
-            (b / 255f).coerceIn(0f, 1f),
-            (a / 255f).coerceIn(0f, 1f)
-        )
-}
-
-private class BorderedTable(borderColor: Color) : Table() {
-
-    private var pixel = makePixelTexture(borderColor)
-
-    var borderSize: Float = 1f
-    var borderLeft: Boolean = true
-    var borderRight: Boolean = true
-    var borderTop: Boolean = true
-    var borderBottom: Boolean = true
-
-    override fun draw(batch: Batch, parentAlpha: Float) {
-        super.draw(batch, parentAlpha)
-        // TODO: similar to figma, add inside, outside and center options for borders
-
-        if (borderTop) {
-            batch.draw(pixel, x, y + height - borderSize, width, borderSize)
-        }
-
-        if (borderBottom) {
-            batch.draw(pixel, x, y, width, borderSize)
-        }
-
-        if (borderLeft) {
-            batch.draw(pixel, x, y, borderSize, height)
-        }
-
-        if (borderRight) {
-            batch.draw(pixel, x + width - borderSize, y, borderSize, height)
-        }
     }
 }
