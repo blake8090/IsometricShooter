727f940d91940d2987ce80f064d6b9b461720032
Blake Edwards <blake8090@gmail.com>
2024-01-28 13:19:43 -0500

    refactor ui class a bit

--- src/main/kotlin/bke/iso/engine/ui/UI.kt
+++ src/main/kotlin/bke/iso/engine/ui/UI.kt
@@ -3,47 +3,20 @@ package bke.iso.engine.ui
 import bke.iso.engine.Event
 import bke.iso.engine.input.Input
 import bke.iso.engine.ui.loading.LoadingScreen
+import com.badlogic.gdx.utils.Array
 import mu.KotlinLogging
 
 class UI(private val input: Input) {
 
     private val log = KotlinLogging.logger {}
-    private val screens = ArrayDeque<UIScreen>()
+
+    private val screens = Array<UIScreen>()
 
     lateinit var loadingScreen: LoadingScreen
         private set
 
     val isLoadingScreenActive: Boolean
         get() = loadingScreen.active
-
-    fun setScreen(screen: UIScreen) {
-        clear()
-        log.debug { "Setting screen to ${screen::class.simpleName}" }
-        screens.addFirst(screen)
-        input.addInputProcessor(screen.stage)
-        input.addControllerListener(screen.controllerNavigation)
-        screen.create()
-        if (input.isUsingController()) {
-            screen.controllerNavigation.start()
-        }
-    }
-
-    private fun clear() {
-        for (screen in screens) {
-            log.debug { "Disposing screen ${screen::class.simpleName}" }
-            input.removeInputProcessor(screen.stage)
-            input.removeControllerListener(screen.controllerNavigation)
-            screen.dispose()
-        }
-        screens.clear()
-    }
-
-    fun setLoadingScreen(screen: LoadingScreen) {
-        log.debug { "Setting loading screen to '${screen::class.simpleName}'" }
-        screen.create()
-        this.loadingScreen = screen
-        log.debug { "Set loading screen to '${screen::class.simpleName}'" }
-    }
 
     fun draw(deltaTime: Float) {
         if (loadingScreen.active) {
@@ -68,9 +41,41 @@ class UI(private val input: Input) {
         }
     }
 
+    fun setScreen(screen: UIScreen) {
+        clear()
+
+        log.debug { "Setting screen to ${screen::class.simpleName}" }
+
+        screens.add(screen)
+        input.addInputProcessor(screen.stage)
+        input.addControllerListener(screen.controllerNavigation)
+        screen.create()
+
+        if (input.isUsingController()) {
+            screen.controllerNavigation.start()
+        }
+    }
+
+    fun setLoadingScreen(screen: LoadingScreen) {
+        log.debug { "Setting loading screen to '${screen::class.simpleName}'" }
+
+        screen.create()
+        this.loadingScreen = screen
+
+        log.debug { "Set loading screen to '${screen::class.simpleName}'" }
+    }
+
     fun dispose() {
+        clear()
+    }
+
+    private fun clear() {
         for (screen in screens) {
+            log.debug { "Disposing screen ${screen::class.simpleName}" }
+            input.removeInputProcessor(screen.stage)
+            input.removeControllerListener(screen.controllerNavigation)
             screen.dispose()
         }
+        screens.clear()
     }
 }
