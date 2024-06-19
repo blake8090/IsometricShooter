3cdded13acf189bf53faa253bfeb11e532973344
Blake Edwards <blake8090@gmail.com>
2024-06-19 10:26:32 -0400

    tweak mission, refactor visibility system and cleanup code

--- src/main/kotlin/bke/iso/game/IsometricShooter.kt
+++ src/main/kotlin/bke/iso/game/IsometricShooter.kt
@@ -7,8 +7,12 @@ import bke.iso.engine.input.ControllerAxisBinding
 import bke.iso.engine.input.ControllerBinding
 import bke.iso.engine.input.KeyBinding
 import bke.iso.engine.input.MouseBinding
+import bke.iso.game.player.RELOAD_ACTION
+import bke.iso.game.player.SHOOT_ACTION
 import bke.iso.game.weapon.WeaponPropertiesCache
 import com.badlogic.gdx.Input
+import com.studiohartman.jamepad.ControllerAxis
+import com.studiohartman.jamepad.ControllerButton
 
 class IsometricShooter : GameInfo() {
 
@@ -32,11 +36,11 @@ class IsometricShooter : GameInfo() {
                 ),
                 "run" to KeyBinding(Input.Keys.SHIFT_LEFT, ButtonState.DOWN),
                 "jump" to KeyBinding(Input.Keys.SPACE, ButtonState.PRESSED),
-                bke.iso.game.player.SHOOT_ACTION to MouseBinding(
+                SHOOT_ACTION to MouseBinding(
                     Input.Buttons.LEFT,
                     ButtonState.DOWN
                 ),
-                bke.iso.game.player.RELOAD_ACTION to KeyBinding(
+                RELOAD_ACTION to KeyBinding(
                     Input.Keys.R,
                     ButtonState.PRESSED
                 ),
@@ -59,32 +63,32 @@ class IsometricShooter : GameInfo() {
         with(game.input.controller) {
             bind(
                 "run" to ControllerBinding(
-                    com.studiohartman.jamepad.ControllerButton.LEFTBUMPER.ordinal,
+                    ControllerButton.LEFTBUMPER.ordinal,
                     ButtonState.DOWN
                 ),
-                "moveX" to ControllerAxisBinding(com.studiohartman.jamepad.ControllerAxis.LEFTX.ordinal),
-                "moveY" to ControllerAxisBinding(com.studiohartman.jamepad.ControllerAxis.LEFTY.ordinal, true),
-                "cursorX" to ControllerAxisBinding(com.studiohartman.jamepad.ControllerAxis.RIGHTX.ordinal),
-                "cursorY" to ControllerAxisBinding(com.studiohartman.jamepad.ControllerAxis.RIGHTY.ordinal),
+                "moveX" to ControllerAxisBinding(ControllerAxis.LEFTX.ordinal),
+                "moveY" to ControllerAxisBinding(ControllerAxis.LEFTY.ordinal, true),
+                "cursorX" to ControllerAxisBinding(ControllerAxis.RIGHTX.ordinal),
+                "cursorY" to ControllerAxisBinding(ControllerAxis.RIGHTY.ordinal),
                 "jump" to ControllerBinding(
-                    com.studiohartman.jamepad.ControllerButton.A.ordinal,
+                    ControllerButton.A.ordinal,
                     ButtonState.PRESSED
                 ),
-                bke.iso.game.player.SHOOT_ACTION to ControllerAxisBinding(com.studiohartman.jamepad.ControllerAxis.TRIGGERRIGHT.ordinal),//ControllerBinding(ControllerButton.RIGHTBUMPER.ordinal, ButtonState.DOWN),
-                bke.iso.game.player.RELOAD_ACTION to ControllerBinding(
-                    com.studiohartman.jamepad.ControllerButton.X.ordinal,
+                SHOOT_ACTION to ControllerAxisBinding(ControllerAxis.TRIGGERRIGHT.ordinal),//ControllerBinding(ControllerButton.RIGHTBUMPER.ordinal, ButtonState.DOWN),
+                RELOAD_ACTION to ControllerBinding(
+                    ControllerButton.X.ordinal,
                     ButtonState.PRESSED
                 ),
                 "crouch" to ControllerBinding(
-                    com.studiohartman.jamepad.ControllerButton.LEFTSTICK.ordinal,
+                    ControllerButton.LEFTSTICK.ordinal,
                     ButtonState.PRESSED
                 ),
                 "useMedkit" to ControllerBinding(
-                    com.studiohartman.jamepad.ControllerButton.DPAD_UP.ordinal,
+                    ControllerButton.DPAD_UP.ordinal,
                     ButtonState.PRESSED
                 ),
                 "openDoor" to ControllerBinding(
-                    com.studiohartman.jamepad.ControllerButton.Y.ordinal,
+                    ControllerButton.Y.ordinal,
                     ButtonState.PRESSED
                 )
             )
--- src/main/kotlin/bke/iso/game/player/PlayerBuildingVisibilitySystem.kt
+++ src/main/kotlin/bke/iso/game/player/PlayerBuildingVisibilitySystem.kt
@@ -6,6 +6,7 @@ import bke.iso.engine.math.Box
 import bke.iso.engine.render.Sprite
 import bke.iso.engine.world.World
 import bke.iso.engine.world.actor.Actor
+import bke.iso.game.door.Door
 
 /**
  * When the player enters a building, hides walls on the south and east sides so that the player is not obscured.
@@ -16,44 +17,36 @@ class PlayerBuildingVisibilitySystem(private val world: World) : System {
         val playerActor = world.actors.find<Player>() ?: return
 
         for (buildingName in world.buildings.getAll()) {
-            checkVisibility(playerActor, buildingName)
+            updateWallVisibility(playerActor, buildingName)
         }
     }
 
-    private fun checkVisibility(playerActor: Actor, buildingName: String) {
+    private fun updateWallVisibility(playerActor: Actor, buildingName: String) {
         val bounds = world.buildings.getBounds(buildingName) ?: return
         val box = playerActor.getCollisionBox() ?: return
 
-        if (bounds.intersects(box)) {
-            hideWalls(buildingName, bounds)
-        } else {
-            showWalls(buildingName, bounds)
+        val alpha =
+            if (bounds.intersects(box)) {
+                0.1f
+            } else {
+                1f
+            }
+
+        for (wall in getSouthAndEastWalls(buildingName, bounds)) {
+            // TODO: add a sprite alpha override component?
+            wall.with<Sprite> { sprite ->
+                sprite.alpha = alpha
+            }
         }
     }
 
-    private fun hideWalls(buildingName: String, bounds: Box) {
-        // TODO: add a wall component and filter actors
-        val walls = world.buildings
+    private fun getSouthAndEastWalls(buildingName: String, bounds: Box) =
+        world.buildings
             .getAllObjects(buildingName)
             .filterIsInstance<Actor>()
-            .filter { wall -> isSouthOrEastWall(wall, bounds) }
-
-        // TODO: add a sprite alpha override component?
-        for (wall in walls) {
-            wall.with<Sprite> { sprite -> sprite.alpha = 0.2f }
-        }
-    }
-
-    private fun showWalls(buildingName: String, bounds: Box) {
-        val walls = world.buildings
-            .getAllObjects(buildingName)
-            .filterIsInstance<Actor>()
-            .filter { wall -> isSouthOrEastWall(wall, bounds) }
-
-        for (wall in walls) {
-            wall.with<Sprite> { sprite -> sprite.alpha = 1f }
-        }
-    }
+            .filter { wall ->
+                isSouthOrEastWall(wall, bounds) && !wall.has<Door>()
+            }
 
     private fun isSouthOrEastWall(actor: Actor, buildingBounds: Box): Boolean {
         val box = actor.getCollisionBox() ?: return false
