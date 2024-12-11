267ddf2273bb322ad8ebe402e3e7de9a1e7cd415
Blake Edwards <blake8090@gmail.com>
2024-12-10 21:59:12 -0500

    use texture2D in shader to fix crash on steam deck

--- assets/game/shaders/color.frag.glsl
+++ assets/game/shaders/color.frag.glsl
@@ -6,7 +6,7 @@ uniform sampler2D u_texture;
 varying vec2 v_texCoords;
 
 void main() {
-    vec4 color = texture(u_texture, v_texCoords);
+    vec4 color = texture2D(u_texture, v_texCoords);
     color.r = u_color.r;
     color.g = u_color.g;
     color.b = u_color.b;
