54ec3b405489d4fbfcb3fa0f17e2c250515f7648
Blake Edwards <blake8090@gmail.com>
2024-12-24 21:43:53 -0500

    remove duplicate style

--- src/main/kotlin/bke/iso/editor/actor/ui/ActorInspectorView.kt
+++ src/main/kotlin/bke/iso/editor/actor/ui/ActorInspectorView.kt
@@ -1,15 +1,12 @@
 package bke.iso.editor.actor.ui
 
-import bke.iso.editor.ui.color
 import bke.iso.engine.collision.Collider
 import bke.iso.engine.render.Sprite
 import bke.iso.engine.world.actor.Component
-import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.ui.TextField
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 
@@ -24,8 +21,6 @@ class ActorInspectorView(private val skin: Skin) {
     private lateinit var editorTable: Table
 
     fun create(): Actor {
-        setup()
-
         root.background = skin.getDrawable("bg")
 
         root.add(Label("Component Editor", skin))
@@ -40,26 +35,6 @@ class ActorInspectorView(private val skin: Skin) {
             .padTop(5f)
 
         return root
-    }
-
-    private fun setup() {
-        skin.add("actorInspector", TextField.TextFieldStyle().apply {
-            font = skin.getFont("default")
-            fontColor = Color.WHITE
-            focusedFontColor = Color.WHITE
-
-            background = skin.newDrawable("pixel", Color.BLACK)
-            focusedBackground = skin.newDrawable("pixel", Color.BLACK)
-
-            cursor = skin.newDrawable("pixel", color(50, 158, 168))
-            selection = skin.newDrawable("pixel", color(50, 158, 168))
-        })
-
-        skin.add("actorInspector", TextButton.TextButtonStyle().apply {
-            font = skin.getFont("default")
-            down = skin.newDrawable("pixel", color(43, 103, 161))
-            over = skin.newDrawable("pixel", color(34, 84, 133))
-        })
     }
 
     fun updateComponent(component: Component) {
@@ -78,7 +53,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(sprite.texture, skin, "actorInspector").apply {
+            TextField(sprite.texture, skin).apply {
                 onChanged {
                     println("changed texture to $text")
                 }
@@ -92,7 +67,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(sprite.offsetX.toString(), skin, "actorInspector").apply {
+            TextField(sprite.offsetX.toString(), skin).apply {
                 onChanged {
                     sprite.offsetX = text.toFloatOrNull() ?: 0f
                 }
@@ -106,7 +81,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(sprite.offsetY.toString(), skin, "actorInspector").apply {
+            TextField(sprite.offsetY.toString(), skin).apply {
                 onChanged {
                     sprite.offsetY = text.toFloatOrNull() ?: 0f
                 }
@@ -120,7 +95,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(sprite.alpha.toString(), skin, "actorInspector").apply {
+            TextField(sprite.alpha.toString(), skin).apply {
                 onChanged {
                     println("changed alpha to $text")
                     sprite.alpha = text.toFloatOrNull() ?: 0f
@@ -135,7 +110,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(sprite.scale.toString(), skin, "actorInspector").apply {
+            TextField(sprite.scale.toString(), skin).apply {
                 onChanged {
                     println("changed scale to $text")
                     sprite.scale = text.toFloatOrNull() ?: 0f
@@ -150,7 +125,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(sprite.rotation.toString(), skin, "actorInspector").apply {
+            TextField(sprite.rotation.toString(), skin).apply {
                 onChanged {
                     println("changed scale to $text")
                     sprite.rotation = text.toFloatOrNull() ?: 0f
@@ -170,7 +145,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(collider.size.x.toString(), skin, "actorInspector").apply {
+            TextField(collider.size.x.toString(), skin).apply {
                 onChanged {
                     collider.size.x = text.toFloatOrNull() ?: 0f
                 }
@@ -184,7 +159,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(collider.size.y.toString(), skin, "actorInspector").apply {
+            TextField(collider.size.y.toString(), skin).apply {
                 onChanged {
                     collider.size.y = text.toFloatOrNull() ?: 0f
                 }
@@ -198,7 +173,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(collider.size.z.toString(), skin, "actorInspector").apply {
+            TextField(collider.size.z.toString(), skin).apply {
                 onChanged {
                     collider.size.z = text.toFloatOrNull() ?: 0f
                 }
@@ -216,7 +191,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(collider.offset.x.toString(), skin, "actorInspector").apply {
+            TextField(collider.offset.x.toString(), skin).apply {
                 onChanged {
                     collider.offset.x = text.toFloatOrNull() ?: 0f
                 }
@@ -230,7 +205,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(collider.offset.y.toString(), skin, "actorInspector").apply {
+            TextField(collider.offset.y.toString(), skin).apply {
                 onChanged {
                     collider.offset.y = text.toFloatOrNull() ?: 0f
                 }
@@ -244,7 +219,7 @@ class ActorInspectorView(private val skin: Skin) {
             .padBottom(5f)
 
         editorTable.add(
-            TextField(collider.offset.z.toString(), skin, "actorInspector").apply {
+            TextField(collider.offset.z.toString(), skin).apply {
                 onChanged {
                     collider.offset.z = text.toFloatOrNull() ?: 0f
                 }
