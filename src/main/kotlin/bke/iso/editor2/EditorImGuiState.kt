4ed1ed04d1d79272bc9d86ec62946514797cd747
Blake Edwards <blake8090@gmail.com>
2025-04-12 22:46:39 -0400

    draw grid

--- src/main/kotlin/bke/iso/editor2/EditorImGuiState.kt
+++ src/main/kotlin/bke/iso/editor2/EditorImGuiState.kt
@@ -8,6 +8,8 @@ import bke.iso.engine.input.ButtonState
 import bke.iso.engine.state.State
 import bke.iso.engine.state.System
 import com.badlogic.gdx.Input
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.math.Vector3
 import imgui.ImGui
 import io.github.oshai.kotlinlogging.KotlinLogging
 
@@ -17,12 +19,53 @@ class EditorImGuiState(override val engine: Engine) : State() {
 
     private val log = KotlinLogging.logger {}
 
+    private var gridWidth = 20
+    private var gridLength = 20
+    private var drawGridForeground = false
+
+    var selectedLayer = 0f
+        private set
+
     override suspend fun load() {
         log.info { "Starting editor" }
 
         engine.ui2.stop()
         engine.input.keyMouse.bindMouse("openContextMenu", Input.Buttons.RIGHT, ButtonState.RELEASED)
     }
+
+    override fun update(deltaTime: Float) {
+        super.update(deltaTime)
+
+        drawGrid()
+    }
+
+    private fun drawGrid() {
+        val shapes = getShapesArray()
+
+        for (x in 0..gridWidth) {
+            shapes.addLine(
+                Vector3(x.toFloat(), 0f, selectedLayer),
+                Vector3(x.toFloat(), gridLength.toFloat(), selectedLayer),
+                0.5f,
+                Color.WHITE
+            )
+        }
+        for (y in 0..gridLength) {
+            shapes.addLine(
+                Vector3(0f, y.toFloat(), selectedLayer),
+                Vector3(gridWidth.toFloat(), y.toFloat(), selectedLayer),
+                0.5f,
+                Color.WHITE
+            )
+        }
+    }
+
+    private fun getShapesArray() =
+        if (drawGridForeground) {
+            engine.renderer.fgShapes
+        } else {
+            engine.renderer.bgShapes
+        }
 
     override fun onFrameEnd() {
         super.onFrameEnd()
@@ -38,7 +81,6 @@ class EditorImGuiState(override val engine: Engine) : State() {
             }
             ImGui.endMainMenuBar()
         }
-
         ImGui.showDemoWindow()
         endImGui()
     }
